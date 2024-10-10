bf6e57066b69c32fa0402a5c034a6b0157ef1db4
John Cortes <jhn_cortes@outlook.com>
2024-10-10 18:20:20 +1100

    Fixed and improved margins and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ import { NotebookPen } from 'lucide-react';
 
 
 export default function Header() {
-  const links = ['Info', 'About Us', 'Contact', 'Book a Consultation'];
+  const links = ['Info', 'About Us', 'Contact', 'Booking'];
   const routes = ['/info', '/about', '/contact', '/booking'];
   const icons = [
     <BookOpen className="w-5 h-5 mr-2"/>, 
@@ -19,66 +19,21 @@ export default function Header() {
   ]
 
   return (
-    <nav class="block w-full max-w-screen-xl px-0 py-0 mx-auto text-white bg-white lg:px-0  mt-0 pt-0">
-      <div class="container flex flex-wrap items-center justify-between mx-auto text-black">
-        <span
-          class="mr-4 block cursor-pointer py-1.5 text-sm text-left font-semibold">
+    <header className="flex flex-row justify-between items-center mx-8">
+      <span className="mr-4 block cursor-pointer py-1.5 text-sm text-left font-semibold">
           <Link to='/'>Wicked Interactive <br></br>Storytelling</Link>
-        </span>
-        <div class="hidden lg:block">
-          <ul class="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-            <li class="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-              {links.map((link, index) => {
-                return (
-                  <Link to={`${routes[index]}`}>
-                    <Button variant='ghost'className="hover:text-violet-600">
-                      {icons[index]}
-                      {link}
-                    </Button>
-                  </Link>
-                );
-            })}
-            </li>
-            {/* <li
-              class="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-    
-    
-              <a href="#" class="flex items-center">
-                Account
-              </a>
-            </li>
-            <li
-              class="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-    
-    
-              <a href="#" class="flex items-center">
-                Blocks
-              </a>
-            </li>
-            <li
-              class="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-    
-    
-              <a href="#" class="flex items-center">
-                Docs
-              </a>
-            </li> */}
-          </ul>
-        </div>
-        <button
-          class="relative ml-auto h-6 max-h-[40px] w-6 max-w-[40px] select-none rounded-lg text-center align-middle text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:hidden"
-          type="button">
-          <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
-          </span>
-        </button>
-      </div>
-    </nav>
-
-    
-
+      </span>
+      <nav className="flex flex-row items-center gap-7 text-muted-foreground text-sm">
+        {links.map((link, index) => {
+          return (
+            <Link to={routes[index]} className="flex hover:text-primary hover:scale-105 transition-all">
+              {icons[index]}
+              {link}
+            </Link>
+          );
+        })}
+      </nav>
+    </header>
   );
 }
 
